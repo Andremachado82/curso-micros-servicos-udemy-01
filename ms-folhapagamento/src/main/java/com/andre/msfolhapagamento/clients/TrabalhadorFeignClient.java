7a9c729243184f0e776b41c8c913a77f774ed09a
André Machado <andrels.machado82@gmail.com>
2024-03-24 18:58:00 -0300

    Implementando FeignClient

---
+++ ms-folhapagamento/src/main/java/com/andre/msfolhapagamento/clients/TrabalhadorFeignClient.java
@@ -0,0 +1,16 @@
+package com.andre.msfolhapagamento.clients;
+
+import com.andre.msfolhapagamento.entities.Trabalhadores;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient(name = "ms-trabalhadores", url = "localhost:8081", path = "/trabalhadores")
+public interface TrabalhadorFeignClient {
+
+    @GetMapping(value = "/{id}")
+    ResponseEntity<Trabalhadores> findById(@PathVariable Long id);
+}
--- ms-folhapagamento/src/main/java/com/andre/msfolhapagamento/services/PagamentoService.java
+++ ms-folhapagamento/src/main/java/com/andre/msfolhapagamento/services/PagamentoService.java
@@ -1,5 +1,6 @@
 package com.andre.msfolhapagamento.services;
 
+import com.andre.msfolhapagamento.clients.TrabalhadorFeignClient;
 import com.andre.msfolhapagamento.entities.Pagamento;
 import com.andre.msfolhapagamento.entities.Trabalhadores;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,18 +15,11 @@ import java.util.Objects;
 @Service
 public class PagamentoService {
 
-    @Value("${ms-trabalhadores.host}")
-    private String hostMsTrabalhadores;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private TrabalhadorFeignClient feignClient;
 
     public Pagamento getPagamento(Long idTrabalhador, Integer dias) {
-        Map<String, String> uriVariaveis = new HashMap<>();
-        uriVariaveis.put("id", idTrabalhador.toString());
-        Trabalhadores trabalhadores =
-                restTemplate.getForObject(hostMsTrabalhadores + "/trabalhadores/{id}", Trabalhadores.class, uriVariaveis);
-
+        Trabalhadores trabalhadores = feignClient.findById(idTrabalhador).getBody();
         if (trabalhadores != null) return new Pagamento(trabalhadores.getNome(), trabalhadores.getRendaDiaria(), dias);
 
         return null;
