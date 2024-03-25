1e4f8fab8fa6b2108653cb3486c712c70091778e
André Machado <andrels.machado82@gmail.com>
2024-03-25 19:44:27 -0300

    Criação e configuração Zuul api gateway

---
+++ ms-api-gateway-zuul/src/test/java/com/andre/msapigatewayzuul/MsApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andre.msapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
