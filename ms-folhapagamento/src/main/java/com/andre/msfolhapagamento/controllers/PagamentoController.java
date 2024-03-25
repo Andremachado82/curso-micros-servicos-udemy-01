d55e8f560745c4518151397eddd56f40a7fae056
André Machado <andrels.machado82@gmail.com>
2024-03-25 19:18:21 -0300

    Config Hystrix tolerancia a falhas

--- ms-folhapagamento/src/main/java/com/andre/msfolhapagamento/controllers/PagamentoController.java
+++ ms-folhapagamento/src/main/java/com/andre/msfolhapagamento/controllers/PagamentoController.java
@@ -2,12 +2,15 @@ package com.andre.msfolhapagamento.controllers;
 
 import com.andre.msfolhapagamento.entities.Pagamento;
 import com.andre.msfolhapagamento.services.PagamentoService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.math.BigDecimal;
 
 @Controller
 @RequestMapping(value = "pagamentos")
@@ -16,8 +19,14 @@ public class PagamentoController {
     @Autowired
     private PagamentoService pagamentoService;
 
+    @HystrixCommand(fallbackMethod = "getPagamentoAlternativo")
     @GetMapping(value = "/{idTrabalhador}/dias/{dias}")
     public ResponseEntity<Pagamento> getPagamento(@PathVariable Long idTrabalhador, @PathVariable Integer dias) {
         return ResponseEntity.ok(pagamentoService.getPagamento(idTrabalhador, dias));
+    }
+
+    public ResponseEntity<Pagamento> getPagamentoAlternativo(Long workerId, Integer days) {
+        Pagamento pagamento = new Pagamento("Brann", new BigDecimal(400), days);
+        return ResponseEntity.ok(pagamento);
     }
 }
